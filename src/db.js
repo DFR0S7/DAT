7a5a7e9299f18a1db3cc4e96c844c18a4ae4dd62
DFR0S7 <fosterd27@gmail.com>
2026-05-05 20:49:50 -0400

    Add websocket transport to Supabase client

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
 // db.js
 import { createClient } from '@supabase/supabase-js';
-
+realtime: { transport: ws };
 export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
