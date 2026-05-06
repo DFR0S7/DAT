6ec23e80cba4c2b7326d2456d5a87ebdb8a8f623
DFR0S7 <fosterd27@gmail.com>
2026-05-05 21:05:03 -0400

    Remove realtime transport configuration

--- src/db.js
+++ src/db.js
@@ -1,4 +1,3 @@
 // db.js
 import { createClient } from '@supabase/supabase-js';
-realtime: { transport: ws };
 export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
